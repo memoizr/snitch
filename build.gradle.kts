863c47a8ed499238df2a88c7f4c78fcf44d8292f
memoizr <memoizrlabs@gmail.com>
2025-03-01 15:24:23 +0100

    Publish to Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     `maven-publish`
     `java-library`
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 allprojects {
-    group = "me.user"
-    version = "1.0-SNAPSHOT"
+    group = "io.github.memoizr"
+    version = "1.0.0"
 
     repositories {
         mavenCentral()
@@ -19,7 +21,6 @@ allprojects {
 subprojects {
     apply {
         plugin("kotlin")
-        plugin("maven-publish")
         plugin("java-library")
     }
 
@@ -34,14 +35,39 @@ subprojects {
         useJUnitPlatform()
     }
 
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = "snitch"
-                version = "1.0"
-                artifactId = project.name
-                from(components["java"])
+    if (project.name != "example") {
+        apply(plugin = "com.vanniktech.maven.publish")
+        mavenPublishing {
+            coordinates("io.github.memoizr", "snitch-${project.name}", "1.0.0")
+
+            pom {
+                name.set("Snitchg")
+                description.set("A web framework for Kotlin.")
+                inceptionYear.set("2020")
+                url.set("https://memoizr.github.io/snitch")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("memoizr")
+                        name.set("memoizr")
+                        url.set("https://github.com/memoizr/")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/memoizr/snitch/")
+                    connection.set("scm:git:git://github.com/memoizr/snitch.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/memoizr/snitch.git")
+                }
             }
+            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+            signAllPublications()
         }
+
     }
 }
--- guides/docusaurus.config.ts
+++ guides/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://memoizr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/snitch/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
